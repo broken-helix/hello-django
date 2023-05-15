a8abb0c76e4aa4555df204f8d77174d3dad5b9e8
broken-helix <>
2023-05-15 10:51:27 +0000

    Fixed error in ALLOWED_HOSTS in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-w69(a*#h0!4e9gi2d6z((
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
